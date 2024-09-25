d4ee8480db84e7fe8bba05221526c44992571ee0
Samuel Des Cormiers <sdcormiers@hotmail.com>
2024-09-25 18:06:34 -0400

    essai de fix le add et que ca chie pas le id mais j'y arrive pas

--- routes/users.js
+++ routes/users.js
@@ -8,13 +8,33 @@ for (let i = 0; i < 10; i++) {
   items.addItem(i, 'Item' + i, Math.floor(Math.random() * 100));
 }
 console.table(items.items);
-/* GET users listing. */
+
 router.get('/', function(req, res, next) {
   res.render('itemsList', { title: 'Items' , items: items });
 })
 .post('/add', function(req, res, next) {
-  items.addItem(items.items.length, req.body.name, req.body.price);
+  var ids = findMissingNumbers(items.items.map(i => i.id));
+  if (ids.length === 0) {
+    ids.push(items.items[items.items.length - 1].id + 1);
+  }
+  items.addItem(ids[0], req.body.name, req.body.price);
   res.render('itemsList', { title: 'Items' , items: items });
-});
+})
+.post('/removebyid', function(req, res, next) {
+  items.removeItemById(parseInt(req.body.id));
+  res.render('itemsList', { title: 'Items' , items: items });
+})
+.post('/removebyname', function(req, res, next) {
+  items.removeItemByName(req.body.name);
+  res.render('itemsList', { title: 'Items' , items: items });
+})
+
+// Fonction trouvée sur https://stackoverflow.com/questions/38460486/find-missing-numbers-in-an-array
+// @param arr : tableau d'entiers
+// @return tableau d'entiers manquants
+function findMissingNumbers(arr) {
+  var sparse = arr.reduce((sparse, i) => (sparse[i]=1,sparse), []);
+  return [...sparse.keys()].filter(i => !sparse[i]);
+}
 
 module.exports = router;
